6fb84eb2d4743d5b65dda6efb4ee110836f698eb
Vipul Singh <vipulsingh.1404@gmail.com>
2025-08-01 23:30:43 +0530

    seo-commit-4

--- component/Main/contact.tsx
+++ component/Main/contact.tsx
@@ -1,13 +1,10 @@
-import { CalcomWidget } from "@calcom/react-widget";
-
 export default function Contact() {
   return (
-    <div>
-      <h1>Welcome to My Website</h1>
-      <CalcomWidget
-        schedulingLink="YOUR_SCHEDULING_LINK_HERE"
-        buttonText="Book my Cal"
-      />
-    </div>
+    <iframe
+      src="https://cal.com/vipulworks"
+      width="100%"
+      height="600"
+      style={{ border: "none" }}
+    ></iframe>
   );
 }
